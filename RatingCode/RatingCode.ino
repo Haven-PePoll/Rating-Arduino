e21ceec640e42655f6c96bbc122fa61b19e6c967
suryam0801 <58582997+suryam0801@users.noreply.github.com>
2022-05-17 12:22:24 +0530

    new code

--- RatingCode/RatingCode.ino
+++ RatingCode/RatingCode.ino
@@ -1,15 +1,7 @@
 #include <AceButton.h>
 using namespace ace_button;
 
-#ifdef ESP32
-// Different ESP32 boards use different pins
-const int LED1_PIN = 2;
-const int LED2_PIN = 19;
-const int LED3_PIN = 14;
-const int LED4_PIN = 13;
-#else
-const int LED_PIN = LED_BUILTIN;
-#endif
+#include "time.h"
 
 #if defined(ESP32)
 #include <WiFi.h>
@@ -21,8 +13,9 @@ const int LED_PIN = LED_BUILTIN;
 #include "addons/TokenHelper.h" //Provide the token generation process info.
 #include "addons/RTDBHelper.h" //Provide the real-time database payload printing info and other helper functions.
 
-#define WIFI_SSID "moto"
-#define WIFI_PASSWORD "1234567890"
+#define WIFI_SSID "SuryasPhone"
+
+#define WIFI_PASSWORD "suryam0801"
 
 #define API_KEY "AIzaSyCOI3KIs3b3JvAY4u4ayqxiZXBbOuiSpBU"
 
@@ -31,19 +24,39 @@ const int LED_PIN = LED_BUILTIN;
 
 #define FIREBASE_PROJECT_ID "thepollingboothmvp"
 
+#define DATABASE_URL "https://thepollingboothmvp-default-rtdb.asia-southeast1.firebasedatabase.app"
+
 //Define Firebase Data object
 FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
 
+String Question = "";
+
+const char* ntpServer = "pool.ntp.org";
+unsigned long epochTime;
+
+
 String pollId;
 unsigned long previousMillis = 0;
 unsigned long interval = 30000;
 
-const int BUTTON1_PIN = 5;//23
-const int BUTTON2_PIN = 18;
-const int BUTTON3_PIN = 23;
-const int BUTTON4_PIN = 25;
+const int BUTTON1_PIN = 13;
+const int BUTTON2_PIN = 25;
+const int BUTTON3_PIN = 23; 
+const int BUTTON4_PIN = 18;
+const int BUTTON5_PIN = 5;
+
+#ifdef ESP32
+// Different ESP32 boards use different pins
+const int LED1_PIN = 32;
+const int LED2_PIN = 27;
+const int LED3_PIN = 14; 
+const int LED4_PIN = 19;
+const int LED5_PIN = 2;
+#else
+const int LED_PIN = LED_BUILTIN;
+#endif
 
 const int LED_ON = HIGH;
 const int LED_OFF = LOW;
@@ -52,6 +65,7 @@ AceButton button1(BUTTON1_PIN);
 AceButton button2(BUTTON2_PIN);
 AceButton button3(BUTTON3_PIN);
 AceButton button4(BUTTON4_PIN);
+AceButton button5(BUTTON5_PIN);
 
 void handleEvent(AceButton*, uint8_t, uint8_t);
 
@@ -64,10 +78,12 @@ void setup() {
   pinMode(LED2_PIN, OUTPUT);
   pinMode(LED3_PIN, OUTPUT);
   pinMode(LED4_PIN, OUTPUT);
+  pinMode(LED5_PIN, OUTPUT);
   pinMode(BUTTON1_PIN, INPUT_PULLUP);
   pinMode(BUTTON2_PIN, INPUT_PULLUP);
   pinMode(BUTTON3_PIN, INPUT_PULLUP);
   pinMode(BUTTON4_PIN, INPUT_PULLUP);
+  pinMode(BUTTON5_PIN, INPUT_PULLUP);
 
   ButtonConfig* buttonConfig = ButtonConfig::getSystemButtonConfig();
   buttonConfig->setEventHandler(handleEvent);
@@ -77,6 +93,8 @@ void setup() {
   buttonConfig->setFeature(ButtonConfig::kFeatureRepeatPress);
 
   connectToWifi();
+  Serial.print("RSSI: ");
+  Serial.println(WiFi.RSSI());
 }
 
 void loop() {
@@ -91,52 +109,66 @@ void loop() {
     previousMillis = currentMillis;
   }
 
-
   // put your main code here, to run repeatedly:
   button1.check();
   button2.check();
   button3.check();
   button4.check();
+  button5.check();
 }
 
 void connectToWifi () {
-  long st = millis();
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
-  Serial.print("Connecting to Wi-Fi");
+  uint32_t notConnectedCounter = 0;
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to WiFi ..");
 
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.println(WiFi.status());
-    Serial.println("Connecting to WiFi..");
+    digitalWrite(LED1_PIN, (millis() / 500) % 2);
+    delay(100);
+    Serial.println("Wifi connecting...");
+    notConnectedCounter++;
+    if (notConnectedCounter > 150) { // Reset board if not connected after 5s
+      Serial.println("Resetting due to Wifi not connecting...");
+      ESP.restart();
+    }
   }
 
-  Serial.println("Connected to the WiFi network");
-  long tm = millis() - st;
-  Serial.print(F("\nConnected to AP in "));
-  Serial.print(tm, DEC);
-  Serial.println();
-
-  Serial.print("Connected to… ");
+  Serial.println("Wifi connected");
   Serial.println(WiFi.localIP());
-  Serial.println();
 
+  digitalWrite(LED1_PIN, LED_ON);
+  delay(3000);
+  digitalWrite(LED1_PIN, LED_OFF);
+  
+  configTime(0, 0, ntpServer);
   //setup firebase
+
+  config.api_key = API_KEY;
+  config.database_url = DATABASE_URL;
+  config.token_status_callback = tokenStatusCallback;
+
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+
 
   Firebase.begin(&config, &auth);
 
   Firebase.reconnectWiFi(true);
 
 
-  //  Serial.println("Getting User POllId");
-  //  while ((auth.token.uid) == "") {
-  //    Serial.print('.');
-  //    delay(1000);
-  //  }
-  //
-  //  pollId = auth.token.uid.c_str();
-  //  Serial.print("User UID: ");
-  //  Serial.println(pollId);
+  Serial.println("Getting User POllId");
+  while ((auth.token.uid) == "") {
+    Serial.print('.');
+    delay(1000);
+  }
+
+  pollId = auth.token.uid.c_str();
+  Serial.print("User UID: ");
+  Serial.println(pollId);
+
+  readFirebase();
 }
 
 // The event handler for both buttons.
@@ -145,22 +177,34 @@ void handleEvent(AceButton* button, uint8_t eventType, uint8_t buttonState) {
     case AceButton::kEventPressed:
       if (button->getPin() == BUTTON1_PIN) {
         digitalWrite(LED1_PIN, LED_ON);
-        updateFirebase("1");
+        epochTime = getTime();
+        updateFirebase(1);
       } else if (button->getPin() == BUTTON2_PIN) {
         digitalWrite(LED1_PIN, LED_ON);
         digitalWrite(LED2_PIN, LED_ON);
-        updateFirebase("2");
+        epochTime = getTime();
+        updateFirebase(2);
       } else if (button->getPin() == BUTTON3_PIN) {
         digitalWrite(LED1_PIN, LED_ON);
         digitalWrite(LED2_PIN, LED_ON);
         digitalWrite(LED3_PIN, LED_ON);
-        updateFirebase("3");
+        epochTime = getTime();
+        updateFirebase(3);
       } else if (button->getPin() == BUTTON4_PIN) {
         digitalWrite(LED1_PIN, LED_ON);
         digitalWrite(LED2_PIN, LED_ON);
         digitalWrite(LED3_PIN, LED_ON);
         digitalWrite(LED4_PIN, LED_ON);
-        updateFirebase("4");
+        epochTime = getTime();
+        updateFirebase(4);
+      } else if (button->getPin() == BUTTON5_PIN) {
+        digitalWrite(LED1_PIN, LED_ON);
+        digitalWrite(LED2_PIN, LED_ON);
+        digitalWrite(LED3_PIN, LED_ON);
+        digitalWrite(LED4_PIN, LED_ON);
+        digitalWrite(LED5_PIN, LED_ON);
+        epochTime = getTime();
+        updateFirebase(5);
       }
       break;
     case AceButton::kEventReleased:
@@ -168,19 +212,24 @@ void handleEvent(AceButton* button, uint8_t eventType, uint8_t buttonState) {
       digitalWrite(LED2_PIN, LED_OFF);
       digitalWrite(LED3_PIN, LED_OFF);
       digitalWrite(LED4_PIN, LED_OFF);
+      digitalWrite(LED5_PIN, LED_OFF);
+      break;
+    case AceButton::kEventLongPressed:
+      digitalWrite(LED1_PIN, LED_ON);
       break;
   }
 }
 
-void updateFirebase(String option) {
+void updateFirebase(int rating) {
   if (Firebase.ready())
   {
-    String documentPath = "pollData/" + pollId + "";
+    String documentPath = "pollData/";
     FirebaseJson content;
 
-    content.set("fields/rating/stringValue", String(option).c_str());
+    content.set("fields/rating/integerValue", rating);
     content.set("fields/location/stringValue", "creamery");
-    content.set("fields/question/stringValue", "how was the food?");
+    content.set("fields/question/stringValue", String(Question).c_str());
+    content.set("fields/timestamp/integerValue", epochTime);
 
     if (Firebase.Firestore.createDocument(&fbdo, FIREBASE_PROJECT_ID, "", documentPath.c_str(), content.raw())) {
       Serial.printf("ok\n%s\n\n", fbdo.payload().c_str());
@@ -189,5 +238,39 @@ void updateFirebase(String option) {
     } else {
       Serial.println(fbdo.errorReason());
     }
+
+    if (Firebase.Firestore.patchDocument(&fbdo, FIREBASE_PROJECT_ID, "", documentPath.c_str(), content.raw(), "")) {
+      Serial.printf("ok\n%s\n\n", fbdo.payload().c_str());
+      Serial.println("UPdate Write");
+      return;
+    } else {
+      Serial.println(fbdo.errorReason());
+    }
+
   }
+}
+
+void readFirebase () {
+  if (Firebase.RTDB.getString(&fbdo, "/Location-Question/Creamery")) {
+
+    if (fbdo.dataTypeEnum() == fb_esp_rtdb_data_type_string) {
+      Question = fbdo.to<String>();
+      Serial.println(fbdo.to<String>());
+    }
+
+  } else {
+    Serial.println(fbdo.errorReason());
+  }
+}
+
+
+unsigned long getTime() {
+  time_t now;
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    //Serial.println("Failed to obtain time");
+    return (0);
+  }
+  time(&now);
+  return now;
 }
